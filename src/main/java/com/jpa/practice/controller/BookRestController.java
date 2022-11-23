d64ad36212e16eb4fc3b2ef70e01d668aa4e2ba9
이소영 <th42500@naver.com>
2022-11-23 14:24:35 +0900

    [Fix] 사용하지 않는 RequestDto 객체 import 제거

--- src/main/java/com/jpa/practice/controller/BookRestController.java
+++ src/main/java/com/jpa/practice/controller/BookRestController.java
@@ -1,12 +1,10 @@
 package com.jpa.practice.controller;
 
-import com.jpa.practice.domain.dto.BookRequestDto;
 import com.jpa.practice.domain.dto.BookResponseDto;
 import com.jpa.practice.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
