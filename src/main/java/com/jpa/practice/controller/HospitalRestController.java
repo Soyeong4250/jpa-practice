874cb2b63bd76b75eb7a004effad990e391668d1
이소영 <th42500@naver.com>
2022-11-23 17:14:56 +0900

    [Feat] HospitalRestController - id를 이용한 병원 상세정보 및 해당 병원의 댓글 조회 기능 추가

---
+++ src/main/java/com/jpa/practice/controller/HospitalRestController.java
@@ -0,0 +1,29 @@
+package com.jpa.practice.controller;
+
+import com.jpa.practice.domain.dto.HospitalResponseDto;
+import com.jpa.practice.service.HospitalService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/hospitals")
+public class HospitalRestController {
+
+    private final HospitalService hospitalService;
+
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalResponseDto> getHospital(@PathVariable Long id) {
+        HospitalResponseDto hospitalResponseDto = hospitalService.getHospital(id);
+        return ResponseEntity.ok().body(hospitalResponseDto);
+    }
+}
