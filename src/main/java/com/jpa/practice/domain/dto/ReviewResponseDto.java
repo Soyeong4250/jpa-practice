95bafa5317646165b9e61f6205d4d1800a26484f
이소영 <th42500@naver.com>
2022-11-24 23:47:12 +0900

    [Refactor] ReviewResponseDto - @Builder 선언

--- src/main/java/com/jpa/practice/domain/dto/ReviewResponseDto.java
+++ src/main/java/com/jpa/practice/domain/dto/ReviewResponseDto.java
@@ -1,12 +1,14 @@
 package com.jpa.practice.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ReviewResponseDto {
     private String title;
     private String content;
