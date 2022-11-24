2b50fbf5b9a58087a90102d2a2f4980422fa61cf
이소영 <th42500@naver.com>
2022-11-24 13:38:04 +0900

    [Feat] ReviewService - Review 등록 기능 추가

---
+++ src/main/java/com/jpa/practice/service/ReviewService.java
@@ -0,0 +1,31 @@
+package com.jpa.practice.service;
+
+import com.jpa.practice.domain.dto.ReviewRequestDto;
+import com.jpa.practice.domain.dto.ReviewResponseDto;
+import com.jpa.practice.domain.entity.Hospital;
+import com.jpa.practice.domain.entity.Review;
+import com.jpa.practice.repository.HospitalRepository;
+import com.jpa.practice.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final HospitalRepository hospitalRepository;
+
+
+    public ReviewService(ReviewRepository reviewRepository, HospitalRepository hospitalRepository) {
+        this.reviewRepository = reviewRepository;
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public ReviewResponseDto add(ReviewRequestDto dto) {
+        Optional<Hospital> hospital = hospitalRepository.findById(dto.getHospitalId());
+        Review review = new Review(dto.getTitle(), dto.getContent(), dto.getUserName(), hospital.get());
+        Review savedReview = reviewRepository.save(review);
+        return new ReviewResponseDto(savedReview.getTitle(), savedReview.getContent(), savedReview.getUserName(),"리뷰 등록 성공");
+    }
+}
