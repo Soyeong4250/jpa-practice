f57fdb659054e8724e5f04e27a405af843897966
이소영 <th42500@naver.com>
2022-11-24 23:41:18 +0900

    [Refactor]HospitalResponseDto 수정
    1. @Builder 선언
    2. message 변수 추가
    3. reviewList 객체 제거

--- src/main/java/com/jpa/practice/domain/dto/HospitalResponseDto.java
+++ src/main/java/com/jpa/practice/domain/dto/HospitalResponseDto.java
@@ -2,25 +2,18 @@ package com.jpa.practice.domain.dto;
 
 import com.jpa.practice.domain.entity.Review;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class HospitalResponseDto {
     private Long id;
     private String name;
     private String address;
-
-    public HospitalResponseDto(Long id, String name, String address) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-    }
-
-    private List<Review> reviewList = new ArrayList<>();
+    private String message;
 }
