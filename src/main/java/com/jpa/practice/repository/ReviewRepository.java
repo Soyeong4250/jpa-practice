dfd3cbe0c9d0344d2454d4de51e66e7e5b5855c2
이소영 <th42500@naver.com>
2022-11-24 23:46:44 +0900

    [Refactor] HospitalId를 이용하여 Review를 찾는 메서드 쿼리 수정

--- src/main/java/com/jpa/practice/repository/ReviewRepository.java
+++ src/main/java/com/jpa/practice/repository/ReviewRepository.java
@@ -1,10 +1,12 @@
 package com.jpa.practice.repository;
 
 import com.jpa.practice.domain.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    public List<Review> findByHospitalId(Long id);
+    Page<Review> findByHospitalId(Long id, Pageable pageable);
 }
