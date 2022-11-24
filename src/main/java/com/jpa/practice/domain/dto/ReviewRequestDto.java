862e622e922cd203c6e734209011a69d8b1444c0
이소영 <th42500@naver.com>
2022-11-24 13:37:35 +0900

    [Feat] Review 요청 정보를 담은 DTO 클래스 추가

---
+++ src/main/java/com/jpa/practice/domain/dto/ReviewRequestDto.java
@@ -0,0 +1,16 @@
+package com.jpa.practice.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewRequestDto {
+
+    private String title;
+    private String content;
+    private String userName;
+    private Long hospitalId;
+}
