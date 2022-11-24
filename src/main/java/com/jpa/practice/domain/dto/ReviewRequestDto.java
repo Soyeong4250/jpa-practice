c9d76ec1d4ceaeed92e3444a1ca86c7ff5bb6612
이소영 <th42500@naver.com>
2022-11-24 23:39:40 +0900

    [Refactor] ReviewRequestDto Builder 추가

--- src/main/java/com/jpa/practice/domain/dto/ReviewRequestDto.java
+++ src/main/java/com/jpa/practice/domain/dto/ReviewRequestDto.java
@@ -1,10 +1,12 @@
 package com.jpa.practice.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewRequestDto {
